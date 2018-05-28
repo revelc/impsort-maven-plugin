f5984c25aa66781776a65b2bac079bfd65937d6d
Tommy Thorsen <tommy.thorsen@gmail.com>
2018-05-28 11:04:03 +0200

    Don't remove imports referenced by Javadoc comments.
    This change adds some code to the function that removes unused imports,
    to ensure that imports referenced by Javadoc comments (such as {@link
    SomeClass}) are not stripped out.
    
    The unit test for removing imports is extended to test this new
    functionality.

--- src/test/resources/UnusedImports.java
+++ src/test/resources/UnusedImports.java
@@ -14,11 +14,24 @@
 
 package net.revelc.code.imp;
 
+import com.foo.Type1;
+import com.foo.Type2;
+import com.foo.Type3;
+import com.foo.Type4;
+import com.foo.Type5;
+import com.foo.Type6;
+import com.foo.Type7;
+import com.foo.Type8;
+import com.foo.Type9;
+import com.foo.Type10;
+
 import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableMap;
 import io.swagger.annotations.ApiOperation;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -27,12 +40,32 @@ import static org.junit.Assert.assertFalse;
 
 import static org.junit.Assert.*;
 
+/**
+ * The import of {@link HashMap} should not be stripped away, since it
+ * is used in this comment.
+ */
 @Component
 public class UnusedImports {
     ImmutableMap immutable;
 
+    /**
+     * The following should also not be removed:
+     *
+     * @see Map
+     */
     public List<Integer> getList() {
         assertFalse(false);
         return null;
     }
+
+	/**
+	 * {@link Type1#method()}
+	 * {@link Type2#method(Type3, Type4)}
+	 * {@link #method(Type5, Type6)}
+	 * {@value Type7#field}
+	 * @see Type8#method()
+	 * @see Type9#method(Type10)
+	 */
+	public void foo() {
+	}
 }
